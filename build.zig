cde0e01f227f9cc201c07c46b73ee1110e6f2877
Stuff7 <cuentafrst@gmail.com>
2025-08-02 00:06:03 -0600

    build

--- build.zig
+++ build.zig
@@ -21,21 +21,21 @@ pub fn build(b: *std.Build) void {
     var name_buf: [NAME.len + 4]u8 = undefined;
     const bin_name = std.fmt.bufPrint(@constCast(&name_buf), "{s}{s}", .{ NAME, suffix }) catch unreachable;
 
-    const exe = b.addExecutable(.{ .name = bin_name, .root_module = main_module });
+    const exe = addBuild(b, main_module, bin_name);
     b.installArtifact(exe);
 
-    const check = b.addExecutable(.{ .name = bin_name, .root_module = main_module });
+    const check = addBuild(b, main_module, bin_name);
     const check_step = b.step("check", "Build for LSP Diagnostics");
     check_step.dependOn(&check.step);
 }
 
-// fn addBuild(b: *std.Build, main_module: *std.Build.Module, bin_name: []const u8) *std.Build.Step.Compile {
-//     const exe = b.addExecutable(.{ .name = bin_name, .root_module = main_module });
-//
-//     exe.root_module.addIncludePath(.{ .cwd_relative = "build/include" });
-//
-//     exe.addObjectFile(.{ .cwd_relative = "build/lib/libavcodec.a" });
-//     exe.linkLibC();
-//
-//     return exe;
-// }
+fn addBuild(b: *std.Build, main_module: *std.Build.Module, bin_name: []const u8) *std.Build.Step.Compile {
+    const exe = b.addExecutable(.{ .name = bin_name, .root_module = main_module });
+
+    exe.root_module.addIncludePath(.{ .cwd_relative = "build/include" });
+
+    exe.addObjectFile(.{ .cwd_relative = "build/lib/libavcodec.a" });
+    exe.linkLibC();
+
+    return exe;
+}
